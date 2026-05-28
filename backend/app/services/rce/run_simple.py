6b06ff83bb60ec95323ce3ec9c5bb9d60f258602
dhruva-nu <dhruva.nu@think41.com>
2026-05-28 23:41:27 +0530

    added logic to run and exec users code

--- backend/app/services/rce/run_simple.py
+++ backend/app/services/rce/run_simple.py
@@ -1,29 +1,30 @@
 from uuid import UUID
+import logging
 
-from fastapi import Depends
-
-from app.dependencies.build_block import get_build_block_service
 from app.services.build_block_service import BuildBlockService
 from .docker import run_code
 
+logger = logging.getLogger(__name__)
 
-# TODO: This will check where to insert the users code in future
+
 async def add_test_code(
     user_code: str,
     build_block_id: UUID,
-    build_block_service: BuildBlockService = Depends(get_build_block_service),
+    build_block_service: BuildBlockService,
 ) -> str:
-
     try:
         block = await build_block_service.get_block(build_block_id)
         test_code = block.test_code
-        to_run = user_code + test_code
+        to_run = test_code.replace("--user-code--", user_code)
+        logger.info(f"to run code = \n{to_run}")
     except Exception as e:
         raise e  # TODO: need to have log and better error handling
 
     return to_run
 
 
-async def run_simple(code: str, language: str, block_id: UUID) -> dict:
-    final_code = await add_test_code(code, block_id)
+async def run_simple(
+    code: str, language: str, block_id: UUID, build_block_service: BuildBlockService
+) -> dict:
+    final_code = await add_test_code(code, block_id, build_block_service)
     return run_code(final_code, language)
